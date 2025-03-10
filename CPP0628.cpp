cffbd15fb1eff01745e1e58cf085309604dd7254
bach50mta <bach50mta@gmail.com>
2025-03-10 17:09:45 +0700

    update by hackerlor 10/3

--- CPP0628.cpp
+++ CPP0628.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<string>
+#include<stdbool.h>
+#include<algorithm>
 
 using namespace std;
 
@@ -14,9 +16,40 @@ public:
         name = id ="";
     }
 
-    friend istream&
+    friend istream&operator>>(istream& mycin, Doanhnghiep&);
+    friend ostream&operator<<(ostream& mycout, Doanhnghiep&);
 };
 
-int main(){
+istream&operator>>(istream& mycin, Doanhnghiep& ds){
+    getline(mycin, ds.id);
+    getline(mycin, ds.name);
+    mycin >> ds.sv;
+    mycin.ignore();
+    return mycin;
+}
 
+ostream&operator<<(ostream& mycout, Doanhnghiep& ds){
+    mycout << ds.id << " " << ds.name << " " << ds.sv << endl;
+    return mycout;
+}
+
+bool cmp(Doanhnghiep x, Doanhnghiep y){
+    if(x.sv != y.sv) return x.sv > y.sv;
+    return x.id < y.id;
+}
+
+void sapxep(Doanhnghiep* ds, int n){
+    sort(ds, ds+n, cmp);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    Doanhnghiep ds[100];
+
+    for(int i=0;i<n; i++) cin >> ds[i];
+    sapxep(ds, n);
+    for(int i=0;i<n; i++) cout << ds[i];
 }
